b31287e6a24b0497097b210fc202ef8b126ba5e2
gapinska <gapinska.mon@gmail.com>
2019-08-16 22:22:33 +0200

    get date for task which status got changed

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,7 @@ class App extends Component{
     tasks.forEach(task =>{
       if (task.id === id){
         task.active = false
+        task.finishDate= new Date().getTime()
       }
     })
     this.setState({
