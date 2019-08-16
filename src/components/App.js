13b38eaafb7f70769438fb2c886e2b2d8a91ac5f
gapinska <gapinska.mon@gmail.com>
2019-08-16 16:01:18 +0200

    the main structure of app created

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import AddTask from './AddTask'
+import AddTask from './AddTask';
+import TaskList from './TaskList';
 
 class App extends Component{
   render() {
@@ -8,6 +9,7 @@ class App extends Component{
       <div className="App">
         TODO APP
         <AddTask/>
+        <TaskList/>
       </div>
     )
   }
---
+++ src/components/TaskList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Task from './Task'
+
+const TaskList = () => {
+    return (
+        <div>
+           <h2> Tasks list</h2>
+           <Task/>
+           <Task/>
+           <Task/>
+        </div>
+
+
+    )
+}
+
+export default TaskList
